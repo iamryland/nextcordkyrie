ce633d2bf51d1eac0d2b645078cac18a32b342a7
Ry <rileyman2004@gmail.com>
2022-01-01 20:51:25 -0500

    hopefully got rid of "on_ready" hanging, and added guild join listener

--- driver/listen.py
+++ driver/listen.py
@@ -3,6 +3,8 @@
 # This file will contain the discord event listeners
 # and the response the bot will have to them
 from nextcord.ext import commands
+
+import runtime
 from runtime import SnowBot
 from warehouse.classes import cGuild
 import nextcord
@@ -27,6 +29,7 @@ class Execution(commands.Cog):
                                        status=nextcord.Status.online)
         for guild in self.bot.guilds:
             self.bot.cguilds.append(cGuild(guild.id, guild.name, self.dba))
+        return
 
     @commands.Cog.listener()
     async def on_guild_update(self, before, after):
@@ -43,6 +46,24 @@ class Execution(commands.Cog):
         cg = self.bot.get_cguild(ctx.guild.id)
         await ctx.send(cg.get_feat())
         await ctx.message.delete()
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        user = await self.bot.fetch_user(int(self.bot.owner_id))
+        user_dm = await user.create_dm()
+        await user_dm.send(f'I joined a new server! Yay!\nJoined server: {guild.name}\nGuild ID: {guild.id}')
+        print(f'{self.bot.user} joined a new Guild! It joined: ({guild.name})(ID:{guild.id})')
+        announce = None
+        if guild.system_channel:
+            announce = guild.system_channel
+        else:
+            for channel in guild.text_channels:
+                if channel.permissions_for(guild.me).send_messages:
+                    announce = channel
+                    break
+        if announce:
+            await announce.send(self.guild_join_msg)
+            self.bot.cguilds.append(cGuild(guild.id, guild.name, self.dba, announce=announce.id))
 
 
 def setup(bot):
