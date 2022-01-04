fc9e7b7ec3449c16dba3ce45b833d0deb614d81a
Ry <rileyman2004@gmail.com>
2022-01-03 23:18:24 -0500

    Added a custom docs dictionary

--- warehouse/classes/sector/_mod.py
+++ warehouse/classes/sector/_mod.py
@@ -5,12 +5,13 @@ from ._sector import Sector
 class Mod(Sector):
     def __init__(self, gid: int):
         super().__init__(gid, 'mod')
-        if self.check_db():
-            self.new_record()
+        if self._check_db():
+            self._new_record()
             self.reports = None
             self.mods = None
-        self._reports = self.retrieve_db('reports')
-        self._mods = self.retrieve_db('mods')
+        self._reports = self._retrieve_db('reports')
+        self._mods = self._retrieve_db('mods')
+        self.docs = {'reports': 'The reports channel for mod events', 'mods': 'The roles allowed to use moderation commands'}
 
     def __str__(self):
         return 'Mod Features'
@@ -25,7 +26,7 @@ class Mod(Sector):
     @reports.setter
     def reports(self, data):
         self._reports = data
-        self.update_db('reports', data)
+        self._update_db('reports', data)
 
     @property
     def mods(self):
@@ -34,4 +35,4 @@ class Mod(Sector):
     @mods.setter
     def mods(self, data):
         self._mods = data
-        self.update_db('mods', data)
+        self._update_db('mods', data)
